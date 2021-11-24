2e97aa0dec834e2ecbb978581b5f4d7003887a73
Erik Tordera Bermejo <erik.tordera@qustodio.com>
2021-11-24 13:48:56 +0100

    perf(Models): Reduced models to a minimun for this task

--- recomendator/recomendations/serializers.py
+++ recomendator/recomendations/serializers.py
@@ -1,10 +1,9 @@
 import datetime
 
 from django_grpc_framework import proto_serializers
-from rest_framework import serializers
 
-from recomendations.models import Book, Author, BookInstance
-from common.pb2 import book_pb2, author_pb2, book_instance_pb2, recomendations_pb2
+from recomendations.models import Book
+from common.pb2 import book_pb2, recomendations_pb2
 
 
 class BookProtoSerializer(proto_serializers.ModelProtoSerializer):
@@ -18,19 +17,3 @@ class BookRecomendationProtoSerializer(proto_serializers.ModelProtoSerializer):
         model = Book
         proto_class = recomendations_pb2.Book
         fields = ['id']
-
-
-class AuthorProtoSerializer(proto_serializers.ModelProtoSerializer):
-    class Meta:
-        model = Author
-        proto_class = author_pb2.Author
-        fields = ['id', 'first_name', 'last_name', 'date_of_birth', 'date_of_death']
-
-
-class BookInstanceProtoSerializer(proto_serializers.ModelProtoSerializer):
-    class Meta:
-        model = BookInstance
-        proto_class = book_instance_pb2.BookInstance
-        fields = ['id', 'book', 'imprint', 'due_back', 'borrower', 'status']
-
-    id = serializers.UUIDField(read_only=True)
