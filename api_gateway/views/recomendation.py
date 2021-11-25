42d2b509518887f8b0a385ad24ec05e42e0cf4ee
Erik Tordera Bermejo <erik.tordera@qustodio.com>
2021-11-25 18:15:26 +0100

    fix(api-gateway): Just 1 recomendation

--- api_gateway/views/recomendation.py
+++ api_gateway/views/recomendation.py
@@ -22,14 +22,6 @@ GRPC_CATALOG_STUB = book_pb2_grpc.BookControllerStub(GRPC_CATALOG_CHANNEL)
 
 recomendations_schema = RecomendationSchema(many=True)
 
-RECOMENDATION_CHANNEL = grpc.insecure_channel(f"{RECOMENDATOR_HOST}:{RECOMENDATOR_PORT}")
-RECOMENDATION_STUB = recomendations_pb2_grpc.RecomendationsControllerStub(RECOMENDATION_CHANNEL)
-
-RECOMENDATOR_ASYNC_HOST = os.getenv("RECOMENDATOR_ASYNC_HOST", "localhost")
-RECOMENDATOR_ASYNC_PORT = os.getenv("RECOMENDATOR_ASYNC_PORT", "50051")
-
-RECOMENDATION_ASYNC_CHANNEL = grpc.insecure_channel(f"{RECOMENDATOR_ASYNC_HOST}:{RECOMENDATOR_ASYNC_PORT}")
-RECOMENDATION_ASYNC_STUB = recomendations_pb2_grpc.RecomendationsControllerStub(RECOMENDATION_ASYNC_CHANNEL)
 
 @bp.get('/<int:user_id>')
 @doc("Get recomendations list")
@@ -37,23 +29,7 @@ RECOMENDATION_ASYNC_STUB = recomendations_pb2_grpc.RecomendationsControllerStub(
 @returns_json
 def get_list(user_id: int):
     try:
-        response = RECOMENDATION_STUB.List(recomendations_pb2.RecomendationsRequest(
-            id=user_id
-        ))
-        for book in response:
-            books.append(MessageToJson(book))
-    except grpc.RpcError as rpc_error:
-        current_app.logger.error(rpc_error.details())
-        raise GRPCException(rpc_error)
-
-    return json.dumps(books)
-
-@bp.get('async/<int:user_id>')
-@returns_json
-def get_list_async(user_id: int):
-    books = []
-    try:
-        response = RECOMENDATION_ASYNC_STUB.List(recomendations_pb2.RecomendationsRequest(
+        book_ids = GRPC_RECOMENDATOR_STUB.List(recomendations_pb2.RecomendationsRequest(
             id=user_id
         ))
         response = [
