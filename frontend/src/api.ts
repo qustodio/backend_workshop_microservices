6a9d3ec04f3c0d92887af41cd44c418ad4b40bc1
QustodioPablo <73693696+QustodioPablo@users.noreply.github.com>
2021-11-27 10:20:14 +0100

    Added charts (#34)
    * Added charts
    
    * Removed ignored helm files

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,23 +1,23 @@
 export const fetchBooks = () =>
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalogs/books`)
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalog/books`)
     .then(res => res.json())
 
 export const fetchAuthors = () =>
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalogs/authors`)
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalog/authors`)
     .then(res => res.json())
 
 export const fetchGenres = () =>
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalogs/genres`)
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalog/genres`)
     .then(res => res.json())
 
 export const fetchLanguages = () =>
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalogs/languages`)
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalog/languages`)
     .then(res => res.json())
 
 export const fetchBook = (bookId: string|undefined) =>
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalogs/books/${bookId}`)
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalog/books/${bookId}`)
     .then(res => res.json())
 
 export const fetchAuthor = (authorId: number) =>
-  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalogs/authors/${authorId}`)
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/catalog/authors/${authorId}`)
     .then(res => res.json())
